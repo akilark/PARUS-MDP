139dbfe0089f09901ac39173c16dee33eb2da601
akilar <akilar97@gmail.com>
2021-07-19 15:50:48 +0700

    feature(CatalogCreater): Реализован первый прототип. Далее необходимо исправить логические ошибки выполнения программы, после этого произвести рефакторинг для оптимизации кода

---
+++ CatalogCreator/DoubleRepair.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CatalogCreator
+{
+	class DoubleRepair
+	{
+		private string[] _RepairSchemeName;
+		private string[] _DoubleRepairSchemeName;
+	}
+}
---
+++ CatalogCreator/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CatalogCreator
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			CatalogCreator catalogCreator = new CatalogCreator();
+			catalogCreator.Create();
+		}
+	}
+}
